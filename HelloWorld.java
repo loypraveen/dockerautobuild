24be6ae6cd03dc56ec6943c41c9fa9e9c7b0fbf5
loypraveen <loypraveen@gmail.com>
2016-07-29 17:05:32 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld {
    public static void main(String[] args) {
-      Prints "Hello, World" in the terminal window.
+      //Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
       System.out.println("Qick Change");
+      System.out.println("Praveen");
    }
 }
